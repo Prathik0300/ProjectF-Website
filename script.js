247df27fab09b9720a3af893425b3e7454e896d1
Prathik Pugazhenthi <prathik0300@gmail.com>
2020-09-28 02:47:13 +0530

    Navbar

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 var preloader = document.getElementById('preloader');
 var body = document.getElementById('body');
+var main = document.getElementById('background');
+var Hamburger = document.getElementsByClassName("isActive");
+var slideNav = document.getElementById("slideNav");
 function exitPreloader(){
     preloader.style.display = 'none';
     body.style.backgroundColor = 'white';
     body.style.overflowX = '';
     body.style.overflowY = '';
-
+    main.style.display = '';
 }
+
+Hamburger[0].addEventListener("click",function(){
+    Hamburger[0].classList.toggle("active");
+    slideNav.classList.toggle("active");
+    slideNav.style.display = "";
+});
+
